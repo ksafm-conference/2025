f4eae8c8d099f6d117830172f20c5898ab0a2f42
leekoby <abc2752@naver.com>
2025-09-23 16:08:40 +0900

    repo 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import NavBar from '@/components/site/NavBar';
-import Footer from '@/components/site/Footer';
+import "./globals.css";
+import type { Metadata } from "next";
+import NavBar from "@/components/site/NavBar";
+import Footer from "@/components/site/Footer";
 
 export const metadata: Metadata = {
-  title: 'KSAFM Conference',
-  description: 'Conference website ',
+  title: "한국농림기상학회 학술대회",
+  description: "한국농림기상학회 학술대회 홈페이지",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang='ko'>
-      <body className='min-h-screen bg-white text-gray-900'>
+    <html lang="ko">
+      <body className="min-h-screen bg-white text-gray-900">
         <NavBar />
         {children}
         <Footer />
