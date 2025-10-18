6d04ff707d57417326d43a870a45f2b4cc44d2b5
leekoby <abc2752@naver.com>
2025-10-18 16:48:04 +0900

    home 내용 수정

--- src/data/home.ts
+++ src/data/home.ts
@@ -1,13 +1,13 @@
 export const home = {
-  society: 'CONFERENCE ON AGRICULTURAL AND FOREST METEOROLOGY',
-  title: '2025 한국농림기상학회 동계학술대회',
-  theme: '극한기상에 따른 농림분야 피해와 대책',
-  dateText: '2025년 12월 04–05일',
-  venueText: '여수 베네치아 호텔&스위트 (전남여수)',
+  society: "CONFERENCE ON AGRICULTURAL AND FOREST METEOROLOGY",
+  title: "2025 한국농림기상학회 동계학술대회",
+  theme: "",
+  dateText: "2025년 12월 04–05일",
+  venueText: "여수 베네치아 호텔&스위트 (전남여수)",
 
   importantDates: [
-    { date: '2025-04-30', label: '초록제출 마감' },
-    { date: '2025-05-20', label: '온라인 등록기간' },
-    { date: '2025-06-24 ~ 25', label: '행사 기간' },
+    { date: "2025-11-14", label: "초록제출 마감(1차)" },
+    { date: "2025-12-03", label: "온라인 등록기간" },
+    { date: "2025-12-04 ~ 05", label: "행사 기간" },
   ],
 };
